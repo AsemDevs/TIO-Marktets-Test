ee2a65381171f9620dd38dc533d5d23409721aea
asem.abdou <asem.abdou@flexistax.com>
2021-09-01 18:27:29 +0300

    more validations

--- js.js
+++ js.js
@@ -7,31 +7,41 @@
 // index of each digit >> isbn[i]
 
 
-function validISBN10(isbn) {
+if(document.getElementById("ex1") != null ) {
 
-    var weighted_sum = 0, i, digit;
-    if (isbn.length < 10) {
-        return "ISBN is less than 10";
+    function isbn_input() {
+        var isbn = prompt("Please enter ISBN");
+        if (isbn != null) {
+            document.getElementById("isbn_value").innerHTML =
+             isbn + " is " + validISBN10(isbn);
+          }
     }
-    if(isbn.includes("X") || isbn.includes("x")) {
-        isbn = isbn.split(""); // Convert it to Array
-        isbn.pop(); // Remove the "X"
-        isbn.push("10"); // Replace it with 10
-    }
-    for (i = 0; i < 10; i++) {
-        digit = isbn[i];
-        weighted_sum += digit * (i+1);
-    }
+    function validISBN10(isbn) {
+        var weighted_sum = 0, i, digit;
+        if (isbn.length < 10) {
+            return "less than 10";
+        }else if(isbn.length > 10) {
+            return "greater than 10";
+        }
+        if(isbn.includes("X") || isbn.includes("x")) {
+            isbn = isbn.split(""); // Convert it to Array
+            isbn.pop(); // Remove the "X"
+            isbn.push("10"); // Replace it with 10
+        }
+        for (i = 0; i < 10; i++) {
+            digit = isbn[i];
+            weighted_sum += digit * (i+1);
+        }
 
-    if(weighted_sum % 11 == 0) {
-        return true;
-    }
-    else {
-        return false
+        if(weighted_sum % 11 == 0) {
+            return true;
+        }
+        else {
+            return false
+        }
     }
 }
-var isbn= "123456789x";
-console.log(validISBN10(isbn));
+
 
 
 
