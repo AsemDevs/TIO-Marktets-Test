f750c3ad68cb290ad7cf2b7b5f0cf4a3f74a2614
asem.abdou <asem.abdou@flexistax.com>
2021-09-04 13:31:49 +0300

    Better Code Js x2

--- js-ex2.js
+++ js-ex2.js
@@ -4,37 +4,36 @@ if(document.getElementById("ex2") != null ) {
     function n_input() {
         var n = prompt("Please enter Number");
         if (n != null) {
-            document.getElementById("ex2_result").innerHTML = "Check the Console";
-            sumSquare(n);
+            document.getElementById("ex2_result").innerHTML = "Check the Console Also<br>" + sumSquare(n);
+            ;
           }
     }
 
     // Functionality Part
-    
     function sumSquare(n) {
-        let all1 = new Array();
-        let all2 = new Array();
-        let all = new Array();
+        let all1 = [];
+        let all2 = [];
+        let all = [];
         let output1 = [];
         let output2 = [];
+        let outputAll = "";
         let count = 0;
         for (i = 0; i * i <= n; i++) {
             for (j = 0; j * j <= n; j++) {
                 if (i * i + j * j == n) {
-                    
-                    all1.push(i);
+                    all1.push(i); // to push every pair to a separate array array
                     all2.push(j);
                     count++
                 }
             }
         }
-        for (let k = 0; k < count/2; k++) { // count/2 to take unique pairs only
+        for (let k = 0; k < count/2; k++) { // count/2 to count unique pairs only
             output1.push(all1[k])
             output2.push(all2[k])
-            console.log([output1[k], output2[k]]);
-            all.push([output1[k], output2[k]]);
-            // all.push([output2[k]]);
+            all.push([output1[k], output2[k]]); // Combine pairs together in 2D array
         }
+        outputAll = JSON.stringify(all);
         console.log("2D Array = ", all);
+        return outputAll;
     } 
 }
--- js.js
+++ js.js
@@ -46,19 +46,20 @@ if(document.getElementById("ex2") != null ) {
     function n_input() {
         var n = prompt("Please enter Number");
         if (n != null) {
-            document.getElementById("ex2_result").innerHTML = "Check the Console";
-            sumSquare(n);
+            document.getElementById("ex2_result").innerHTML = "Check the Console Also<br>" + sumSquare(n);
+            ;
           }
     }
 
     // Functionality Part
     
     function sumSquare(n) {
-        let all1 = new Array();
-        let all2 = new Array();
-        let all = new Array();
+        let all1 = [];
+        let all2 = [];
+        let all = [];
         let output1 = [];
         let output2 = [];
+        let outputAll = "";
         let count = 0;
         for (i = 0; i * i <= n; i++) {
             for (j = 0; j * j <= n; j++) {
@@ -73,11 +74,11 @@ if(document.getElementById("ex2") != null ) {
         for (let k = 0; k < count/2; k++) { // count/2 to take unique pairs only
             output1.push(all1[k])
             output2.push(all2[k])
-            console.log([output1[k], output2[k]]);
             all.push([output1[k], output2[k]]);
-            // all.push([output2[k]]);
         }
+        outputAll = JSON.stringify(all);
         console.log("2D Array = ", all);
+        return outputAll;
     } 
 }
 
