4b49f53bbb40874ac0974b22ee410d5fe2a26a3f
asem.abdou <asem.abdou@flexistax.com>
2021-09-04 11:03:53 +0300

    Deploy React App

--- api_react/src/App.js
+++ api_react/src/App.js
@@ -3,8 +3,6 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.js';
 
-
-
 class App extends Component {
   
   state = {
@@ -26,28 +24,30 @@ class App extends Component {
     return (
       
        <div className="container">
-        <div className="col-xs-12">
-        <h1>My Todo's</h1>
-        <ul className="list-group">
-        {this.state.todos.map((todo) => (
-            <li className={todo.completed ? 'text-white bg-success list-group-item' : 'text-dark bg-light list-group-item'} key={todo.id}>
-              <h5>{todo.id}</h5>
-              <h6 className="card-subtitle mb-2">
-              { todo.completed &&
-                <span>
-                Completed
-                </span>
-              }
-              { !todo.completed &&
-                <span>
-                  Pending
-                </span>
-              }
-              </h6>
-              <p className="card-text">{todo.title}</p>
-            </li>
-        ))}
-        </ul>
+        <div className="row">
+          <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+          <h1>My Todo's</h1>
+          <ul className="list-group">
+          {this.state.todos.map((todo) => (
+              <li className={todo.completed ? 'text-white bg-success list-group-item' : 'text-dark bg-light list-group-item'} key={todo.id}>
+                <h5>{todo.id}</h5>
+                <h6 className="card-subtitle mb-2">
+                { todo.completed &&
+                  <span>
+                  Completed
+                  </span>
+                }
+                { !todo.completed &&
+                  <span>
+                    Pending
+                  </span>
+                }
+                </h6>
+                <p className="card-text">{todo.title}</p>
+              </li>
+          ))}
+          </ul>
+          </div>
         </div>
        </div>
     );
